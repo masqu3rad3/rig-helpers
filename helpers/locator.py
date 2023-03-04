fa34ce2752b448b2f24bb28884967f965c3f458a
Arda Kutlu <ardakutlu@gmail.com>
2023-03-04 15:15:40 +0000

    locate shelf button added

--- helpers/locator.py
+++ helpers/locator.py
@@ -6,7 +6,7 @@ from trigger.library.naming import unique_name
 
 def locate(objects=None, constraint=False):
     """Create locators matching the pivot point of selected object"""
-    objects = objects or cmds.ls(sl=True, type="transform")
+    objects = objects or cmds.ls(selection=True, type="transform")
     if not isinstance(objects, (tuple, list)):
         objects = [objects]
     locator_list = []
